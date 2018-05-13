0f343b7247be43863c853e947f9d6691f8a081d2
안 지훈 <angh5080@gmail.com>
2018-05-13 09:16:59 +0000

    Client Server 구현중

--- src/kr/co/coders/ajh/server/Client.java
+++ src/kr/co/coders/ajh/server/Client.java
@@ -1,4 +1,13 @@
 package kr.co.coders.ajh.server;
 
+import java.net.Socket;
+import kr.co.coders.ajh.server.runnable.RecvRunnable;
+import kr.co.coders.ajh.server.runnable.SendRunnable;
+
 public class Client {
+	private Socket mSocket = null;
+	private RecvRunnable mRecvRunnable = null;
+	private SendRunnable mSendRunnable = null;
+	
+	
 }
