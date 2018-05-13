6c59005b67da3fc6ff5d72c2f5c3c27b40afa61e
안 지훈 <angh5080@gmail.com>
2018-05-13 09:18:47 +0000

    Server 구현중

--- src/kr/co/coders/ajh/server/Server.java
+++ src/kr/co/coders/ajh/server/Server.java
@@ -1,7 +1,47 @@
 package kr.co.coders.ajh.server;
 
-public class Server {
-	public Server ( ) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import kr.co.coders.ajh.server.runnable.AcceptRunnable;
+import kr.co.coders.ajh.server.listener.onAcceptListener;
+import kr.co.coders.ajh.server.listener.onClosedSocketListener;
+import kr.co.coders.ajh.server.listener.onRecvCompleteListener;
+
+public class Server implements onAcceptListener , onClosedSocketListener , 
+onRecvCompleteListener {
+	
+	private ThreadPoolExecutor mThreadPoolExcutor = null;
+	private ServerSocket mServerSocket = null;
+	private AcceptRunnable mAcceptRunnable = null;
+	private HashMap<Integer , Client> mClientHash = null;
+	
+	public Server ( int port ) {
+		try {
+			mServerSocket = new ServerSocket(port);
+			mThreadPoolExcutor =(ThreadPoolExecutor)Executors.newFixedThreadPool(10);
+			mAcceptRunnable = new AcceptRunnable(mServerSocket , this);
+			mClientHash = new HashMap<>();
+		} catch ( IOException e ) {
+			e.printStackTrace();	
+		}
+	}
+	@Override
+	public void onRecvComplete ( byte[] data , int size , int from ) {
 		
-	}	
+	}
+	
+	@Override
+	public void onClosedSocket ( int hashCode ) {
+	
+	}
+	
+	@Override
+	public void onAccept ( Socket socket ) {
+		
+	}
 }
